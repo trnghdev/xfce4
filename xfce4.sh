473c959922b57ed42da494153790ef82278d4b27
trnghdev <105575826+trnghdev@users.noreply.github.com>
2022-06-13 15:40:55 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -5,11 +5,8 @@ unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp -region ap 3389 &>/dev/null &
-echo Downloading File From akuh.net
 echo "===================================="
 echo "Wait 2 Minutes"
-echo "This is only for education"
-echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
 sudo apt-get update > /dev/null 2>&1
 echo "===================================="
@@ -36,8 +33,6 @@ sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "More  Free VPS akuh.net"
 echo "Don't close this tab RDP runs 12 hours"
-echo "Keep support akuh.net thank you"
 echo "===================================="
 sleep 43210
